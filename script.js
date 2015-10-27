684863e1945f92031f632afbdf011dfca328d400
Britton Walker <bwalker1801@gmail.com>
2015-10-26 21:12:19 -0400

    Flipcard function without winning logic

--- script.js
+++ script.js
@@ -7,18 +7,32 @@ $( document ).ready(function() {
   //Make sure only two cards can be flipped at a time = 1 turn
   //cardCounter = 0; +1
   var cardCounter = 0;
+
   function flipCard(){
-    if(cardCounter < 2){
+    if(cardCounter < 1){
       $(this).toggleClass('hide');
+      firstFlip = $(this).attr("class");
       cardCounter++;
+      console.log(firstFlip);
+    } else if(cardCounter < 2){
+        $(this).toggleClass('hide');
+        secondFlip = $(this).attr("class");
+        cardCounter++;
+     console.log(secondFlip);
+     console.log(cardCounter);
+    } if (firstFlip === secondFlip) {
+      cardCounter = 0;
     }
-    console.log(cardCounter);
   }
-    //if even (%2), checkWinner/compareCards
-    //else hide image
+
+  //if even (%2), checkWinner/compareCards
+  //else hide image
 
   //Check if cards are a match
 
+  function compareCards(card1, card2){
+
+  }
   //Compare two classes; function compareCards(card1, card2)
       //if match cards are permanently flipped; card1.class === card2.class
       //else hide cards
