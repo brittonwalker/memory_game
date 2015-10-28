fb982a93408c333a2054231af251c25a0a0a83e9
Britton Walker <bwalker1801@gmail.com>
2015-10-28 12:13:56 -0400

    Make reset button and 'finish' styling

--- script.js
+++ script.js
@@ -1,37 +1,74 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-  $('.cards').on('click', flipCard);
+  //Variables/////////////////////////////
+  var classes = [ 'blue_wall', 'teal_wall', 'white_wall', 'yellow_wall', 'navy_wall', 'green_wall', 'pink_wall', 'red_wall', 'blue_wall', 'teal_wall', 'white_wall', 'yellow_wall', 'navy_wall', 'green_wall', 'pink_wall', 'red_wall' ];
 
+  var hide = ['hide', 'hide', 'hide', 'hide', 'hide', 'hide', 'hide', 'hide', 'hide', 'hide', 'hide', 'hide', 'hide', 'hide', 'hide', 'hide']
+
+  var cardClass = ['cards', 'cards', 'cards', 'cards', 'cards', 'cards', 'cards', 'cards', 'cards', 'cards', 'cards', 'cards', 'cards', 'cards', 'cards', 'cards'];
+
+  var cardCounter = 0;
+
+  var score = 0;
+  /////////////////////////////////////////
+
+  //Card Click Event//////////////////////
+  $('.cards').on('click', flipCard);
+  //
+
+  //Reset Event/Function//////////////////
+  $('.button').on('click', function(){
+    classes.sort( randomize );
+    $('.cards').each(function(i, val) {
+        $(this).removeClass();
+        $(this).addClass(cardClass[i]);
+        $(this).addClass(classes[i]);
+        $(this).addClass(hide[i]);
+        score = 0;
+    });
+  });
+  /////////////////////////////////////////
+
+  //Randomize Board///////////////////////
   function randomize() {
     return (Math.round(Math.random())-0.5);
   }
+  /////////////////////////////////////////
 
-  var classes = [ 'blue_wall', 'teal_wall', 'white_wall', 'yellow_wall', 'navy_wall', 'green_wall', 'pink_wall', 'red_wall', 'blue_wall', 'teal_wall', 'white_wall', 'yellow_wall', 'navy_wall', 'green_wall', 'pink_wall', 'red_wall' ];
-  classes.sort( randomize );
-  $('.cards').each(function(i, val) {
-      $(this).addClass(classes[i]);
-  });
+  //Check for Winner
+  function checkWinner(){
+    if(score == 8)
+    alert('You my friend, are a champion!');
+  }
+  /////////////////////////////////////////
 
-  var cardCounter = 0;
-  var score = 0;
+  //Comparing Two Cards////////////////////
+  function compareCards(card1, card2){
+    if(card1.attr('class') == card2.attr('class')){
+      score++;
+      cardCounter = 0;
+    } else if(card1.attr('class') != card2.attr('class')){
+      card1.toggleClass('hide');
+      card2.toggleClass('hide');
+      cardCounter = 0;
+    }
+    checkWinner();
+  }
+  /////////////////////////////////////////
 
+
+  //The Game/////////////////////////////////////////
   function flipCard(){
     if(cardCounter < 1){
       $(this).toggleClass('hide');
-      // firstFlip = $(this).attr("class");
       firstFlip = $(this);
       cardCounter++;
-      console.log(cardCounter);
-      console.log(firstFlip);
     } else if(cardCounter < 2){
         if ($(this).hasClass('hide')) {
           $(this).toggleClass('hide');
-          // secondFlip = $(this).attr("class");
           secondFlip = $(this);
           cardCounter++;
-          console.log(secondFlip);
-          console.log(cardCounter);
         }
       event.preventDefault();
     }
@@ -41,23 +78,6 @@ $( document ).ready(function() {
       }, 400)
     }
   }
+  //////////////////////////////////////////////////
 
-  function compareCards(card1, card2){
-    if(card1.attr('class') == card2.attr('class')){
-      score += 1;
-      cardCounter = 0;
-      console.log('match');
-      console.log(score);
-    } else if(card1.attr('class') != card2.attr('class')){
-      card1.toggleClass('hide');
-      card2.toggleClass('hide');
-      cardCounter = 0;
-    }
-    checkWinner();
-  }
-
-  function checkWinner(){
-    if(score == 8)
-    alert('Yeeee son! you won!');
-  }
 });
